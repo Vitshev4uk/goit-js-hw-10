ed783c447ba5cd8af396d09a59e8e44fa2944ced
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-04-15 21:49:36 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.getElementById('search-box');
+console.log(input);
+const countryList = document.querySelector('.country-list');
+console.log(countryList);
+const countryInfo = document.querySelector('.country-info')
+console.log(countryInfo);
+
+input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
+
+function searchCountries(event) {
+    event.preventDefault();
+}
