f222471eb71f6322b82915b7d5eb7e0aa63895f9
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-04-16 21:44:11 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -6,14 +6,63 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 const input = document.getElementById('search-box');
-console.log(input);
 const countryList = document.querySelector('.country-list');
-console.log(countryList);
 const countryInfo = document.querySelector('.country-info')
-console.log(countryInfo);
 
 input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
 
 function searchCountries(event) {
     event.preventDefault();
-}
+    const inputValue = event.target.value.trim();
+    if (!inputValue) {
+        countryList.innerHTML = '';
+    } if (!inputValue) {
+        countryInfo.innerHTML = '';
+    };
+
+    fetchCountries(inputValue).then(country => {
+    if (country.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (country.length >= 2 && country.length <= 10) {
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+        listCountry(country);
+    } else {
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = '';
+        countryCard(country);
+    };
+}).catch((error) => {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+})
+};
+
+function listCountry(country) {
+    const markupCountryList = country.map(({ name, flags }) => {
+        return `<li class="list-country">
+    <img class="img-flag" src="${flags.svg}" alt="flag" width="50px">
+    <h2 class="country-name">${name.official}</h2>
+    </li>`
+    }).join('');
+    return countryList.insertAdjacentHTML('beforeend', markupCountryList);   
+};
+
+function countryCard(country) {
+    const markupCardCounyty = country.map(({ name, capital, population, flags, languages }) => {
+        const lang = Object.values(languages);
+        return `<div class="country-card">
+        <div class="county-main-info">
+           <img class="img-flag" src="${flags.svg}" alt="flag" width="50px">
+           <h2 class="country-name">${name.official}</h2>
+        </div>
+        <ul class="list-country-info">
+           <li class="item-country-info">${capital}</li>
+           <li class="item-country-info">${population}</li>
+           <li class="item-country-info">${lang}</li>
+        </ul>
+        </div>`
+        
+    }).join('');
+    return countryInfo.insertAdjacentHTML('beforeend', markupCardCounyty);
+};
+
